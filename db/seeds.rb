0c9cb04a61fe909a476e793583b4b3c650eecde6
Matthew Coleman <logical.zero@gmail.com>
2012-12-30 18:53:31 -0600

    db: seeds: added some test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+### test data
+
+users = User.create([
+  { name: 'Matthew', pass: 'test', email: 'matt@test.com', username: 'matthew' },
+  { name: 'Joe', pass: 'test', email: 'joe@test.com', username: 'joe' },
+  { name: 'Tamara', pass: 'test', email: 'tamara@test.com', username: 'tamara' },
+  { name: 'Richard', pass: 'test', email: 'rich@test.com', username: 'richard' },
+  { name: 'Elaine', pass: 'test', email: 'elaine@test.com', username: 'elaine' }])
