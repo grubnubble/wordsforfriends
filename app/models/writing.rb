e57ddd11b5f02ba27a6f279ded5515f0008889d5
Matthew Coleman <logical.zero@gmail.com>
2012-12-30 18:54:10 -0600

    app: models: writing: removed booleans from presence check

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,5 +1,7 @@
 class Writing < ActiveRecord::Base
   attr_accessible :author_id, :body, :friends_r, :global_r, :title
 
-  validates :author_id, :body, :friends_r, :global_r, :presence => true
+  validates :author_id, :title, :body, :presence => true
+
+  belongs_to :user, :foreign_key => 'author_id'
 end
