71af5912722afddfc742279232961b37fd357d82
Matthew Coleman <logical.zero@gmail.com>
2013-01-13 21:13:07 -0600

    db: migrate: add email_key to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130101004655) do
+ActiveRecord::Schema.define(:version => 20130112182141) do
 
   create_table "friendships", :id => false, :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20130101004655) do
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
     t.string   "password_salt"
+    t.string   "email_key"
   end
 
   create_table "writing_tag_joins", :force => true do |t|
