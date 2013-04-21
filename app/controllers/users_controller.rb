39de4b59bdeb0497da356e02d083c06a4fcd3369
Matthew Coleman <logical.zero@gmail.com>
2013-04-21 12:17:26 -0500

    activation email message edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,8 @@ class UsersController < ApplicationController
         # activation email
         UserMailer.activate_email( @user).deliver
 
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to root_url, 
+          notice: 'Cool! You should expect an email from us soon (for verification).' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
