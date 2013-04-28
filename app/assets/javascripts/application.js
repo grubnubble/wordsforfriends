3185a8015b23943813e39d6dc8f240c286fe6bbe
Matthew Coleman <logical.zero@gmail.com>
2013-04-27 19:33:58 -0500

    writing form enhancements, autoexpand textarea

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,63 @@
 //= require_tree .
 
 $(document).foundation();
+
+$(document).ready( function() {
+
+  // auto-expanding textarea for writing
+  $('textarea#writing_body').autoheight();
+
+});
+
+$.fn.autoheight = function() {
+  
+  return this.filter( 'textarea').each( function() {
+
+    var textarea_text_linecount = 0;
+    var line_counter = 0;
+    var newline_regexp = /\n/g;
+    var base_height = $(this).css( 'height').match(/^\d+/)[0];
+    var height_diff = parseFloat( 
+      $(this).css( 'line-height').match(/^\d+/)[0])*3;
+    
+    // set initial height
+    if( $(this).val().match( newline_regexp)) {
+      $(this).css({ 'height' : 
+        Math.max( 
+          Math.floor( height_diff *
+            ( $(this).val().match( newline_regexp).length / 2)),
+          base_height)+"px" }); 
+    }
+
+    // auto expand/ contract
+    $(this).on( 'keyup change', function() {
+      
+      var textarea_height = $(this).height();
+      var current_textarea_text_linecount = 
+        $(this).val().match( newline_regexp);
+
+      // removing a line
+      if( $(this).val().length === 0) {
+        $(this).animate({ 'height': base_height+"px" });
+      } else if( current_textarea_text_linecount < textarea_text_linecount) { 
+        line_counter--;
+        if( line_counter < -1) {
+          $(this).stop().
+            animate({ 'height': 
+              Math.max( base_height, textarea_height-height_diff)+"px" });
+          line_counter = 0;
+        }
+      // adding a line 
+      } else if( current_textarea_text_linecount > textarea_text_linecount) { 
+        line_counter++;
+        if( line_counter > 1) {
+          $(this).stop().
+            animate({ 'height': (textarea_height+height_diff)+"px" });
+          line_counter = 0;
+        }
+      }
+
+      textarea_text_linecount = current_textarea_text_linecount;
+    });
+  });
+};
